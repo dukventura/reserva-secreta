3d38b5c541b3e16916d5c22a64bc7ba89bb48f47
dukventura <126628386+dukventura@users.noreply.github.com>
2026-08-05 17:51:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/react-refresh' // ou o plugin que estiver usando
 
 export default defineConfig({
   base: '/guia-prime/', // <--- ADICIONE ESSA LINHA AQUI
+  {
+  "name": "guia-prime",
+  "homepage": "https://github.io",
+  "private": true,
+  ...
+}
+
   plugins: [react()],
   // mantenha o restante das configurações que já existirem abaixo
 })
